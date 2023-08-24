7af5f7ea6b5e79ad854d0590de3ed8552aeac118
helenmdev <helen.publicidad@gmail.com>
2023-08-24 13:54:05 -0500

    CORS config

--- routes/accounts.js
+++ routes/accounts.js
@@ -66,13 +66,14 @@ router.post(
       } else {
         next(err);
       }
-    }
-    const origin = req.headers.origin;
+      const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
       res.header("Access-Control-Allow-Origin", origin);
     }
     res.header("Access-Control-Allow-Methods", "POST");
     res.header("Access-Control-Allow-Headers", "Content-Type");
+    }
+    
   }
 );
 
